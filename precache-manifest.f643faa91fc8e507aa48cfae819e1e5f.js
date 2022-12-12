ce9344dbbdfc21af99877671a7a905f577091d37
Norbert <sadawa.norbert@gmail.com>
2022-12-13 00:04:38 +0100

    Updates

---
+++ precache-manifest.f643faa91fc8e507aa48cfae819e1e5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "affac17814ef74384739",
+    "url": "/login-system-fb/static/css/main.e7c3ce23.chunk.css"
+  },
+  {
+    "revision": "affac17814ef74384739",
+    "url": "/login-system-fb/static/js/main.affac178.chunk.js"
+  },
+  {
+    "revision": "2417c2f0c36462021922",
+    "url": "/login-system-fb/static/js/1.2417c2f0.chunk.js"
+  },
+  {
+    "revision": "0e3c9dba91a20fe57fd6",
+    "url": "/login-system-fb/static/js/runtime~main.0e3c9dba.js"
+  },
+  {
+    "revision": "acad590cc25d28f93e9c2d1afe5f0080",
+    "url": "/login-system-fb/index.html"
+  }
+];
